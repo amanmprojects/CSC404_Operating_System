2335ee5e22d3e00f7fe612102eaa06257406f8d6
Aman Mehtar <127136587+amanmprojects@users.noreply.github.com>
2025-02-24 06:26:31 +0000

    Implement Banker's Algorithm for resource allocation and safety checking

---
+++ BankersAlgorithm.java
@@ -0,0 +1,108 @@
+public class BankersAlgorithm{
+    public static void main(String[] args) {
+        int processes = 5, resources = 3;
+        int[][] allocation= {
+            {0, 1, 0},
+            {2, 0, 0},
+            {3, 0, 2},
+            {2, 1, 1},
+            {0, 0, 2}
+        };
+        int[][] max = {
+            {7, 5, 3},
+            {3, 2, 2},
+            {9, 0, 2},
+            {2, 2, 2},
+            {4, 3, 3}
+        };
+        int[][] need = new int[processes][resources];
+        for (int i = 0; i < processes; i++) {
+            for (int j = 0; j < resources; j++) {
+            need[i][j] = max[i][j] - allocation[i][j];
+            }
+        }
+        
+        int[] available = {
+            3, 3, 2
+        };
+        if (isSafe(processes, resources, allocation, max, available)) {
+            System.out.println("System is in a safe state.");
+        } else {
+            System.out.println("System is not in a safe state.");
+        }
+
+        int[] request = {0, 2, 0};
+        requestResources(1, request, allocation, max, available);
+    }
+
+    public static boolean isSafe(int processes, int resources, int[][] allocation, int[][] max, int[] available) {
+        int[][] need = new int[processes][resources];
+        for (int i = 0; i < processes; i++) {
+            for (int j = 0; j < resources; j++) {
+                need[i][j] = max[i][j] - allocation[i][j];
+            }
+        }
+
+        boolean[] finish = new boolean[processes];
+        int[] safeSequence = new int[processes];
+        int count = 0;
+
+        while (count < processes) {
+            boolean found = false;
+            for (int p = 0; p < processes; p++) {
+                if (!finish[p]) {
+                    int j;
+                    for (j = 0; j < resources; j++) {
+                        if (need[p][j] > available[j]) {
+                            break;
+                        }
+                    }
+                    if (j == resources) {
+                        for (int k = 0; k < resources; k++) {
+                            available[k] += allocation[p][k];
+                        }
+                        safeSequence[count++] = p;
+                        finish[p] = true;
+                        found = true;
+                        break;
+                    }
+                }  
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        System.out.print("Safe sequence is: ");
+        for (int i = 0; i < processes ; i++) {
+            System.out.print(safeSequence[i] + " ");
+        }
+        System.out.println();
+        return true;
+    }
+
+    public static void requestResources(int process, int[] request, int[][] allocation, int[][] max, int[] available) {
+        for (int i = 0; i < available.length; i++) {
+            if (request[i] > available[i]) {
+                System.out.println("Request cannot be granted.");
+                return;
+            }
+        }
+
+        for (int i = 0; i < available.length; i++) {
+            available[i] -= request[i];
+            allocation[process][i] += request[i];
+            max[process][i] -= request[i];
+        }
+
+        if (isSafe(allocation.length, allocation[0].length, allocation, max, available)) {
+            System.out.println("Request granted.");
+        } else {
+            for (int i = 0; i < available.length; i++) {
+                available[i] += request[i];
+                allocation[process][i] -= request[i];
+                max[process][i] += request[i];
+            }
+            System.out.println("Request cannot be granted.");
+        }
+    }
+}
